0926a846b95e69673dd1502bcf7b0f568e7781e9
Hardware <contact@meshup.net>
2019-06-30 09:12:00 +0200

    Ajout d'un mode nuit

---
+++ hitmanstat.us/Lib/local/js/night-mode.js
@@ -0,0 +1,22 @@
+﻿function toggleNight() {
+    localStorage.setItem('mode', (localStorage.getItem('mode') || 'light') === 'night' ? 'light' : 'night');
+    if (localStorage.getItem('mode') === 'night') {
+        document.querySelector('body').classList.add('night-mode');
+        document.querySelector("#night-link").innerHTML = "Light mode";
+    } else {
+        document.querySelector('body').classList.remove('night-mode');
+        document.querySelector("#night-link").innerHTML = "Night mode";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if ((localStorage.getItem('mode') || 'light') === 'night') {
+        document.querySelector('body').classList.add('night-mode');
+        document.querySelector("#night-link").innerHTML = "Light mode";
+    } else {
+        document.querySelector('body').classList.remove('night-mode');
+        document.querySelector("#night-link").innerHTML = "Night mode";
+    }
+});
+
+
